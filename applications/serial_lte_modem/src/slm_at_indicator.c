744c915577c147c549e8a76d16240a7db6600d9c
Larry Tsai <larry.tsai@nordicsemi.no>
2021-01-29 20:54:15 +0800

    Init commit of SLM Indicator PoC

---
+++ applications/serial_lte_modem/src/slm_at_indicator.c
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+#include "slm_at_indicator.h"
---
+++ applications/serial_lte_modem/src/slm_at_indicator.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#ifndef SLM_AT_INDICATOR_
+#define SLM_AT_INDICATOR_
+
+/** @} */
+
+#endif /* SLM_AT_INDICATOR_ */
