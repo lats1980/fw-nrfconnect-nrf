dedeca89b15ec975c6b5786024216b5bf503a20a
Larry Tsai <larry.tsai@nordicsemi.no>
2021-11-18 16:28:58 +0800

    coap server change

--- samples/openthread/coap_server/src/coap_server.c
+++ samples/openthread/coap_server/src/coap_server.c
@@ -5,23 +5,89 @@
  */
 
 #include <zephyr.h>
-#include <dk_buttons_and_leds.h>
+//#include <dk_buttons_and_leds.h>
 #include <logging/log.h>
 #include <net/openthread.h>
 #include <openthread/thread.h>
 
 #include "ot_coap_utils.h"
 
+#include <drivers/uart.h>
+
 LOG_MODULE_REGISTER(coap_server, CONFIG_COAP_SERVER_LOG_LEVEL);
 
-#define OT_CONNECTION_LED DK_LED1
-#define PROVISIONING_LED DK_LED3
-#define LIGHT_LED DK_LED4
+//#define OT_CONNECTION_LED DK_LED1
+//#define PROVISIONING_LED DK_LED3
+//#define LIGHT_LED DK_LED4
+#define AT_CMD_AT				"AT\r"
+//#define AT_CMD_CRLF				"\r\n"
+#define UART_BUF_SIZE 20
+#define UART_WAIT_FOR_BUF_DELAY K_MSEC(50)
+#define UART_RX_TIMEOUT 50
+
+static const struct device *uart;
+static struct k_work_delayable uart_work;
+
+struct uart_data_t {
+	void *fifo_reserved;
+	uint8_t  data[UART_BUF_SIZE];
+	uint16_t len;
+};
+
+static K_FIFO_DEFINE(fifo_uart_tx_data);
+static K_FIFO_DEFINE(fifo_uart_rx_data);
 
 static struct k_work provisioning_work;
 
 static struct k_timer led_timer;
 static struct k_timer provisioning_timer;
+
+//static char data[] = {"AT\r\n"};
+
+#if 1
+static uint8_t slm_uart_data_tx(char *data, uint16_t len)
+{
+	int err;
+
+#if 1
+	for (uint16_t pos = 0; pos != len;) {
+		struct uart_data_t *tx = k_malloc(sizeof(*tx));
+
+		if (!tx) {
+			LOG_WRN("Not able to allocate UART send data buffer");
+			return 1;
+		}
+
+		/* Keep the last byte of TX buffer for potential LF char. */
+		size_t tx_data_size = sizeof(tx->data) - 1;
+
+		if ((len - pos) > tx_data_size) {
+			tx->len = tx_data_size;
+		} else {
+			tx->len = (len - pos);
+		}
+
+		memcpy(tx->data, &data[pos], tx->len);
+
+		pos += tx->len;
+
+		/* Append the LF character when the CR character triggered
+		 * transmission from the peer.
+		 */
+		if ((pos == len) && (data[len - 1] == '\r')) {
+			tx->data[tx->len] = '\n';
+			tx->len++;
+		}
+
+		err = uart_tx(uart, tx->data, tx->len, SYS_FOREVER_MS);
+		if (err) {
+			k_fifo_put(&fifo_uart_tx_data, tx);
+		}
+	}
+#endif
+	return 0;
+}
+#endif
 
 static void on_light_request(uint8_t command)
 {
@@ -29,18 +95,18 @@ static void on_light_request(uint8_t command)
 
 	switch (command) {
 	case THREAD_COAP_UTILS_LIGHT_CMD_ON:
-		dk_set_led_on(LIGHT_LED);
+		//dk_set_led_on(LIGHT_LED);
 		val = 1;
 		break;
 
 	case THREAD_COAP_UTILS_LIGHT_CMD_OFF:
-		dk_set_led_off(LIGHT_LED);
+		//dk_set_led_off(LIGHT_LED);
 		val = 0;
 		break;
 
 	case THREAD_COAP_UTILS_LIGHT_CMD_TOGGLE:
 		val = !val;
-		dk_set_led(LIGHT_LED, val);
+		//dk_set_led(LIGHT_LED, val);
 		break;
 
 	default:
@@ -84,7 +150,7 @@ static void on_led_timer_expiry(struct k_timer *timer_id)
 
 	ARG_UNUSED(timer_id);
 
-	dk_set_led(PROVISIONING_LED, val);
+	//dk_set_led(PROVISIONING_LED, val);
 	val = !val;
 }
 
@@ -92,9 +158,9 @@ static void on_led_timer_stop(struct k_timer *timer_id)
 {
 	ARG_UNUSED(timer_id);
 
-	dk_set_led_off(PROVISIONING_LED);
+	//dk_set_led_off(PROVISIONING_LED);
 }
-
+/*
 static void on_button_changed(uint32_t button_state, uint32_t has_changed)
 {
 	uint32_t buttons = button_state & has_changed;
@@ -103,7 +169,7 @@ static void on_button_changed(uint32_t button_state, uint32_t has_changed)
 		k_work_submit(&provisioning_work);
 	}
 }
-
+*/
 static void on_thread_state_changed(uint32_t flags, void *context)
 {
 	struct openthread_context *ot_context = context;
@@ -113,17 +179,173 @@ static void on_thread_state_changed(uint32_t flags, void *context)
 		case OT_DEVICE_ROLE_CHILD:
 		case OT_DEVICE_ROLE_ROUTER:
 		case OT_DEVICE_ROLE_LEADER:
-			dk_set_led_on(OT_CONNECTION_LED);
+			//dk_set_led_on(OT_CONNECTION_LED);
+			slm_uart_data_tx(AT_CMD_AT, strlen(AT_CMD_AT));
 			break;
 
 		case OT_DEVICE_ROLE_DISABLED:
 		case OT_DEVICE_ROLE_DETACHED:
 		default:
-			dk_set_led_off(OT_CONNECTION_LED);
+			//dk_set_led_off(OT_CONNECTION_LED);
 			deactivate_provisionig();
 			break;
 		}
 	}
+}
+
+static void uart_cb(const struct device *dev, struct uart_event *evt, void *user_data)
+{
+	ARG_UNUSED(dev);
+
+	static uint8_t *current_buf;
+	static size_t aborted_len;
+	static bool buf_release;
+	struct uart_data_t *buf;
+	static uint8_t *aborted_buf;
+
+	switch (evt->type) {
+	case UART_TX_DONE:
+		if ((evt->data.tx.len == 0) ||
+		    (!evt->data.tx.buf)) {
+			return;
+		}
+
+		if (aborted_buf) {
+			buf = CONTAINER_OF(aborted_buf, struct uart_data_t,
+					   data);
+			aborted_buf = NULL;
+			aborted_len = 0;
+		} else {
+			buf = CONTAINER_OF(evt->data.tx.buf,
+					   struct uart_data_t,
+					   data);
+		}
+
+		k_free(buf);
+
+		buf = k_fifo_get(&fifo_uart_tx_data, K_NO_WAIT);
+		if (!buf) {
+			return;
+		}
+
+		if (uart_tx(uart, buf->data, buf->len, SYS_FOREVER_MS)) {
+			LOG_WRN("Failed to send data over UART");
+		}
+
+		break;
+
+	case UART_RX_RDY:
+		buf = CONTAINER_OF(evt->data.rx.buf, struct uart_data_t, data);
+		buf->len += evt->data.rx.len;
+		buf_release = false;
+
+	LOG_INF("rx %d", buf->len);
+
+		if (buf->len == UART_BUF_SIZE) {
+			k_fifo_put(&fifo_uart_rx_data, buf);
+		}
+
+		break;
+
+	case UART_RX_DISABLED:
+		buf = k_malloc(sizeof(*buf));
+		if (buf) {
+			buf->len = 0;
+		} else {
+			LOG_WRN("Not able to allocate UART receive buffer");
+			k_work_reschedule(&uart_work, UART_WAIT_FOR_BUF_DELAY);
+			return;
+		}
+
+		uart_rx_enable(uart, buf->data, sizeof(buf->data),
+			       UART_RX_TIMEOUT);
+
+		break;
+
+	case UART_RX_BUF_REQUEST:
+		buf = k_malloc(sizeof(*buf));
+		if (buf) {
+			buf->len = 0;
+			uart_rx_buf_rsp(uart, buf->data, sizeof(buf->data));
+		} else {
+			LOG_WRN("Not able to allocate UART receive buffer");
+		}
+
+		break;
+
+	case UART_RX_BUF_RELEASED:
+		buf = CONTAINER_OF(evt->data.rx_buf.buf, struct uart_data_t,
+				   data);
+		if (buf_release && (current_buf != evt->data.rx_buf.buf)) {
+			k_free(buf);
+			buf_release = false;
+			current_buf = NULL;
+		}
+
+		break;
+
+	case UART_TX_ABORTED:
+			if (!aborted_buf) {
+				aborted_buf = (uint8_t *)evt->data.tx.buf;
+			}
+
+			aborted_len += evt->data.tx.len;
+			buf = CONTAINER_OF(aborted_buf, struct uart_data_t,
+					   data);
+
+			uart_tx(uart, &buf->data[aborted_len],
+				buf->len - aborted_len, SYS_FOREVER_MS);
+
+		break;
+
+	default:
+		break;
+	}
+}
+
+static void uart_work_handler(struct k_work *item)
+{
+	struct uart_data_t *buf;
+
+	buf = k_malloc(sizeof(*buf));
+	if (buf) {
+		buf->len = 0;
+	} else {
+		LOG_WRN("Not able to allocate UART receive buffer");
+		k_work_reschedule(&uart_work, UART_WAIT_FOR_BUF_DELAY);
+		return;
+	}
+
+	uart_rx_enable(uart, buf->data, sizeof(buf->data), UART_RX_TIMEOUT);
+}
+
+static int uart_init(void)
+{
+	int err;
+	struct uart_data_t *rx;
+
+	uart = device_get_binding(DT_LABEL(DT_NODELABEL(uart0)));
+	if (!uart) {
+		LOG_ERR("UART binding failed");
+		return -ENXIO;
+	}
+
+	rx = k_malloc(sizeof(*rx));
+	if (rx) {
+		rx->len = 0;
+	} else {
+		return -ENOMEM;
+	}
+
+	k_work_init_delayable(&uart_work, uart_work_handler);
+
+	err = uart_callback_set(uart, uart_cb, NULL);
+	if (err) {
+		return err;
+	}
+
+	return uart_rx_enable(uart, rx->data, sizeof(rx->data),
+			      UART_RX_TIMEOUT);
 }
 
 void main(void)
@@ -137,12 +359,14 @@ void main(void)
 
 	k_work_init(&provisioning_work, activate_provisioning);
 
+	uart_init();
+
 	ret = ot_coap_init(&deactivate_provisionig, &on_light_request);
 	if (ret) {
 		LOG_ERR("Could not initialize OpenThread CoAP");
 		goto end;
 	}
-
+/*
 	ret = dk_leds_init();
 	if (ret) {
 		LOG_ERR("Could not initialize leds, err code: %d", ret);
@@ -154,7 +378,7 @@ void main(void)
 		LOG_ERR("Cannot init buttons (error: %d)", ret);
 		goto end;
 	}
-
+*/
 	openthread_set_state_changed_cb(on_thread_state_changed);
 	openthread_start(openthread_get_default_context());
 
