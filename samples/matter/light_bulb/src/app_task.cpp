d8497b0a22563dcfc7083254cd0d01184fae8b7e
Larry Tsai <larry.tsai@nordicsemi.no>
2023-12-05 14:05:58 +0800

    matter: samples: light: add subscription window handling
    Add window handling to negotiate reporting interval with controller.

--- samples/matter/light_bulb/src/app_task.cpp
+++ samples/matter/light_bulb/src/app_task.cpp
@@ -32,6 +32,11 @@
 #include "ota_util.h"
 #endif
 
+#ifdef CONFIG_CHIP_ICD_SUBSCRIPTION_HANDLING
+#include <app/InteractionModelEngine.h>
+#endif
+
+
 #include <dk_buttons_and_leds.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
@@ -214,6 +219,10 @@ CHIP_ERROR AppTask::Init()
 	app::SetAttributePersistenceProvider(&gDeferredAttributePersister);
 	ConfigurationMgr().LogDeviceConfig();
 	PrintOnboardingCodes(chip::RendezvousInformationFlags(chip::RendezvousInformationFlag::kBLE));
+
+#ifdef CONFIG_CHIP_ICD_SUBSCRIPTION_HANDLING
+	chip::app::InteractionModelEngine::GetInstance()->RegisterReadHandlerAppCallback(&GetICDUtil());
+#endif
 
 	/*
 	 * Add CHIP event handler and start CHIP thread.
